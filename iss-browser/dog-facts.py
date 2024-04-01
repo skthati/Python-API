4667f84810574298ad2a0c32cf3ca71beedc5fae
Sandeep Thati <97585808+skthati@users.noreply.github.com>
2024-04-01 16:58:47 +1300

    update html

--- iss-browser/dog-facts.py
+++ iss-browser/dog-facts.py
@@ -3,23 +3,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-url = "https://dogapi.dog/api/v2/facts"
 
-response = requests.get(url)
-
-print(response)
-
-data = response.json()
-
-#print(data)
-
-random_fact = data['data'][0]['attributes']['body']
-
-print(random_fact)
+def new_dog_fact():
+    url = "https://dogapi.dog/api/v2/facts"
+    response = requests.get(url)
+    print(response)
+    data = response.json()
+    new_random_fact = data['data'][0]['attributes']['body']
+    print(new_random_fact)
+    return new_random_fact
 
 
 @app.route("/")
 def index():
+    random_fact = new_dog_fact()
     return render_template('index.html', random_fact = random_fact)
 
 if __name__ == "__main__":
