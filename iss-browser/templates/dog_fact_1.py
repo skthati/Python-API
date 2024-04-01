27b75abe1f3b2585985bda12a85a09d60f2c485f
Sandeep Thati <97585808+skthati@users.noreply.github.com>
2024-04-01 16:10:17 +1300

    html file

--- iss-browser/templates/dog_fact_1.py
+++ iss-browser/templates/dog_fact_1.py
@@ -1,4 +1,5 @@
 import requests
+import webbrowser
 
 url = "https://dogapi.dog/api/v2/facts"
 
@@ -7,10 +8,18 @@ response = requests.get(url)
 data = response.json()
 
 random_data_1 = data['data'][0]['attributes']['body']
-print(random_data_1)
+#print(random_data_1)
 
-html_file = open('index.html', "w")
+#html_file = open('index.html', "r")
+#print(html_file)
+#new_html_file = html_file.read()
+#print(new_html_file)
 
-new_html_file = html_file.read()
-print(new_html_file)
+with open('index.html', 'r') as file:
+    #data1 = file.read().rstrip()
+    data1 = file.read().replace("*random_fact*", random_data_1)
+
+print(data1)
+
+webbrowser.open_new_tab('index.html')
 
