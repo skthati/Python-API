c9f8464ccbac14ac0c08856a332cf83c069b1cf7
Sandeep Thati <97585808+skthati@users.noreply.github.com>
2024-04-01 20:16:17 +1300

    update

--- iss-browser/dog-facts.py
+++ iss-browser/dog-facts.py
@@ -20,4 +20,4 @@ def index():
     return render_template('index.html', random_fact = random_fact)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8001, debug=True)
